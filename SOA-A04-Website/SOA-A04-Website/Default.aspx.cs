1110c340a0207af42ccbb05fb17b97b42928ed60
Aaron <aschryver-cc@conestogac.on.ca>
2015-11-13 01:14:53 -0500

    Added basic skeletal website

---
+++ SOA-A04-Website/SOA-A04-Website/Default.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SOA_A04_Website
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //Stupid name... I know...
+        protected void goToNextScreen(int exeType)
+        {
+            Session["exeType"] = exeType;
+            Server.Transfer("pages/Screen2.aspx");
+        }
+
+
+        protected void searchBtn_Click(object sender, EventArgs e)
+        {
+            goToNextScreen(0);
+        }
+
+        protected void updateBtn_Click(object sender, EventArgs e)
+        {
+            goToNextScreen(1);
+        }
+
+        protected void insertBtn_Click(object sender, EventArgs e)
+        {
+            goToNextScreen(2);
+        }
+
+        protected void deleteBtn_Click(object sender, EventArgs e)
+        {
+            goToNextScreen(3);
+        }
+    }
+}
---
+++ SOA-A04-Website/SOA-A04-Website/pages/Screen3.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Screen3.aspx.cs" Inherits="SOA_A04_Website.pages.Screen3" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Results Page</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <h3>Results Page For Testing</h3>
+    <div>
+        <asp:TextBox ID="outputTxtBox" runat="server" TextMode="MultiLine" 
+                    BorderStyle="Solid" Height="480px" Width="640px">
+        </asp:TextBox>
+    </div>
+    </form>
+</body>
+</html>
