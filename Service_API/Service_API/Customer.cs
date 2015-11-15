500d08ab285ed7e5872caa1a2151f55270cec2ca
Anthony-Salutari <anthony17183@gmail.com>
2015-11-14 20:46:21 -0500

    create the service api

---
+++ Service_API/Service_API/Customer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Service_API
+{
+    class Customer
+    {
+        int customerID { get; set; }
+        string firstName { get; set; }
+        string lastName { get; set; }
+        string phoneNumber { get; set; }
+
+        // default constructor
+        public Customer()
+        {
+
+        }
+
+        public Customer(int customerID, string firstName, string lastName, string phoneNumber)
+        {
+            this.customerID = customerID;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.phoneNumber = phoneNumber;
+        }
+    }
+}
