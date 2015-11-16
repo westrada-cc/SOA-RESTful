430139b2bf19ab886de0f5d039fdcd68835f31cb
Anthony-Salutari <anthony17183@gmail.com>
2015-11-16 17:35:43 -0500

    Finished the DELETE methods without proper response handling

--- Service_API/Service.cs
+++ Service_API/Service.cs
@@ -156,22 +156,30 @@ namespace Service_API
         #region delete methods
         public void deleteCustomer(int customerID, string firstName, string lastName, string phoneNumber)
         {
-            Customer customer = new Customer(customerID, firstName, lastName, phoneNumber);
+            client = new HttpClient();
+
+            var response = client.DeleteAsync(SERVICE_URL + "customers/" + customerID.ToString());
         }
 
         public void deleteProduct(int productID, string productName, float price, float productWeight, bool soldOut)
         {
-            Product product = new Product(productID, productName, price, productWeight, soldOut);
+            client = new HttpClient();
+
+            var response = client.DeleteAsync(SERVICE_URL + "products/" + productID.ToString());
         }
 
         public void deleteOrder(int orderID, int customerID, string poNumber, string orderDate)
         {
-            Order order = new Order(orderID, customerID, poNumber, orderDate);
+            client = new HttpClient();
+
+            var response = client.DeleteAsync(SERVICE_URL + "orders/" + orderID.ToString());
         }
 
         public void deleteCart(int orderID, int productID, int quantity)
         {
-            Cart cart = new Cart(orderID, productID, quantity);
+            client = new HttpClient();
+
+            var response = client.DeleteAsync(SERVICE_URL + "carts/" + orderID);
         }
         #endregion
 
