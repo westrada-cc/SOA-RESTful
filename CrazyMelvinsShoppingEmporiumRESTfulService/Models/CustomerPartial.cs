0bb8a3f4ef33d0acd6d589161ce106ddf6c2ba49
Streebor <stefanvasiljevic@me.com>
2015-11-16 11:23:49 -0500

    Added some validation for Customer class. (Work in progress)

---
+++ CrazyMelvinsShoppingEmporiumRESTfulService/Models/CustomerPartial.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CrazyMelvinsShoppingEmporiumRESTfulService.Models
+{
+    [MetadataType(typeof(CustomerMetadata))]
+    public partial class Customer
+    {
+        public const int MaxFirstNameLength = 50;
+
+        private sealed class CustomerMetadata
+        {
+            [Required]
+            public int custID { get; set; }
+            [Required]
+            [StringLength(MaxFirstNameLength)]
+            public string firstName { get; set; }
+            [Required]
+            public string lastName { get; set; }
+            [Required]
+            public string phoneNumber { get; set; }
+        }
+    }
+}
